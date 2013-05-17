c153fd44405322b0f7c2cd9a140b491bb86424fc
edeustace <ed.eustace@gmail.com>
2013-05-17 13:10:55 +0200

    added grunt script + coffee

--- app.js
+++ app.js
@@ -1,57 +1,19 @@
 var fs = require('fs'),
   http = require('http'),
-  launcher = require('./app/launcher'),
-  express = require('express'),
-  tokenGenerator = require('./app/tokenGenerator');
+  express = require('express');
 
 var app = express();
 
-app.use(require('connect-assets')());
-app.set('views', __dirname + '/views');
+app.use(require('connect-assets')({ src: __dirname + '/client/assets'}));
+app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client/public'));
 app.use(express.bodyParser());
 
 app.get('/', function(req, res) {
-  res.redirect('/player');
+  res.render('index', {title: "Index"});
 });
 
-app.get('/player', function(req, res) {
-  res.render('player-form');
-});
-
-app.get('/profile', function(req, res) {
-  res.render('profile-form');
-});
-
-app.get('/hello', function(req, res) {
-  res.send('hello');
-});
-app.get('/testpage', function(req, res) {
-  res.render('testpage');
-});
-
-app.get('/make-token', function(req,res){
- res.render('make-token')
-});
-
-app.post('/make-token', function(req, res){
-  tokenGenerator.makeToken( req.body.corespringUrl, req.body.clientId, req.body.clientSecret, function(err,data){
-    var result = {accessToken : data};
-    console.log( result );
-    res.render('token-result', result );
-  })
-});
-
-app.post('/launch-player',
-launcher.encryptOptions,
-launcher.buildPlayerOptions,
-launcher.launchPlayer);
-
-app.post('/launch-profile',
-launcher.encryptOptions,
-launcher.buildPlayerOptions,
-launcher.launchProfile);
 
 // start server
 var port = process.env.PORT || 5000;
