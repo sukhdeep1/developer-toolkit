de0eb9309b1153f45834c867070ec0a028ec150d
edeustace <ed.eustace@gmail.com>
2013-05-17 17:48:20 +0200

    try using app.get('port')

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.get('/', function(req, res) {
 
 // start server
 var port = process.env.PORT || 5000;
-http.createServer(app).listen(port, function() {
+app.set('port', port);//process.env.PORT || 3000);
+http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + port);
 });
