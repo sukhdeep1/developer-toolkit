c760347f92105bbdd284a75589ac3a09514c89d3
edeustace <ed.eustace@gmail.com>
2013-05-29 18:34:43 +0200

    flesh out search form spec

--- client/spec/specs/controllers/search-form-spec.js
+++ client/spec/specs/controllers/search-form-spec.js
@@ -2,19 +2,56 @@
 
 describe('search-form', function () {
   var scope = null;
+
+  var mockCollectionData = null;
+  var mockCollections = {
+    get: function (p, success, error) {
+
+      if(p.access_token == "success"){
+        success(mockCollectionData);
+      } else {
+        error("error");
+      }
+    }
+  };
+
   beforeEach(function () {
-    scope = helpers.controller.initController('SearchForm', {'Collections': {}, 'ItemSearch': {}})
+    scope = helpers.controller.initController('SearchForm', {'Collections': mockCollections, 'ItemSearch': {}})
   });
 
   it('should init', function () {
     expect(scope).toNotBe(null);
   });
 
-  it('should reset form', function(){
+  it('should reset form', function () {
     scope.keyword = "blah";
     expect(scope.keyword).toEqual("blah");
     scope.resetForm();
     expect(scope.keyword).toBe(null);
+  });
+
+  it('should update collections', function () {
+    spyOn(scope, '$emit');
+    scope.updateCollections(null);
+    expect(scope.collections).toEqual([]);
+    mockCollectionData = [1];
+    scope.updateCollections("success");
+    expect(scope.collections).toEqual([1]);
+    expect(scope.$emit).toHaveBeenCalled();
+    expect(scope.$emit.calls.length).toEqual(1);
+    expect(scope.$emit).toHaveBeenCalledWith('apiCallSucceeded', 'collections');
+  });
+
+
+  it('should empty the collection if it fails', function () {
+    spyOn(scope, '$emit');
+    scope.collections = [1,2,3];
+    mockCollectionData = [1];
+    scope.updateCollections("fail");
+    expect(scope.collections).toEqual([]);
+    expect(scope.$emit).toHaveBeenCalled();
+    expect(scope.$emit.calls.length).toEqual(1);
+    expect(scope.$emit).toHaveBeenCalledWith('apiCallFailed', 'collections');
   });
 });
 
